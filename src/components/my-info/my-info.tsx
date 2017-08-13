4fc4136250c72ec0495568e97d8a6cef203d9a14
Fernando del olmo <fer.olmo92@gmail.com>
2017-08-13 13:42:00 +0200

    feat(): Added my info section

--- src/components/my-info/my-info.tsx
+++ src/components/my-info/my-info.tsx
@@ -9,7 +9,16 @@ export class MyInfo {
 
     render() {
         return (
-            <p>My info</p>
+            <div class="my-info-container">
+                <p>I'm a</p>
+                <label class="role">Frontend developer</label>
+                <p>based on Madrid, Spain. 
+                    I studied computer engineering degree in Spain and learned by my self 
+                    the technologies i use now to work. 
+                    I'm interested in everything related to the web, 
+                    mostly Web Components and Progressive Web Apps.
+                </p>
+            </div>
         );
     }
 }
