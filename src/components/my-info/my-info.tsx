af872bd0bb7d9c7062c8982e4c7e6b828dd3112e
Fernando del olmo <fer.olmo92@gmail.com>
2017-08-13 16:23:56 +0200

    fix(): Update my info section

--- src/components/my-info/my-info.tsx
+++ src/components/my-info/my-info.tsx
@@ -13,10 +13,11 @@ export class MyInfo {
                 <p>I'm a</p>
                 <label class="role">Frontend developer</label>
                 <p>based on Madrid, Spain. 
-                    I studied computer engineering degree in Spain and learned by my self 
-                    the technologies i use now to work. 
+                    I studied computer engineering degree and learned by my self 
+                    the technologies i use now to work like Angular and Ionic. 
                     I'm interested in everything related to the web, 
-                    mostly Web Components and Progressive Web Apps.
+                    mostly Web Components and Progressive Web Apps and 
+                    i like to learn new things constantly and try them in side projects.
                 </p>
             </div>
         );
