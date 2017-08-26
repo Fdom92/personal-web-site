40eca4722e5a2b4a0b79b87e3005249b816b9992
Fernando del olmo <fer.olmo92@gmail.com>
2017-08-26 21:26:16 +0200

    feat(): Added my hobbies section

--- src/components/my-hobbies/my-hobbies.tsx
+++ src/components/my-hobbies/my-hobbies.tsx
@@ -9,7 +9,13 @@ export class MyHobbies {
 
     render() {
         return (
-            <p>My hobbies</p>
+            <div class="hobbies">
+                <img src="../assets/travel.svg" alt="Travel"/>
+                <img src="../assets/water-activities.svg" alt="Water Activities"/>
+                <img src="../assets/snowboard.svg" alt="Snowboard"/>
+                <img src="../assets/coding.svg" alt="Coding"/>
+                <img src="../assets/films.svg" alt="Films"/>
+            </div>
         );
     }
 }
